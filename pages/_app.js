9c8df58d3003ea6e8b581e03da8a8005d655b9f5
Tauseef Fayyaz <tauseeffayyaz@gmail.com>
2023-07-10 14:21:03 +0500

    testing commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
-import { UserProvider } from "@auth0/nextjs-auth0/client";
-import Head from "next/head";
-import "../styles/styles.css";
-
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import Head from 'next/head';
+import '../styles/styles.css';
+//To be removed.
 function App({ Component, pageProps }) {
   return (
     <UserProvider>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Auth0 Next.js Sample</title>
-        
       </Head>
       <Component {...pageProps} />
     </UserProvider>
